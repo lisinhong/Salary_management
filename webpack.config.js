8ccb2d655c142c400c83064f2332a19eb6f3b858
alanli <alan12141020@gmail.com>
2018-08-07 15:41:54 +0800

    change output path

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 	entry: ['./src/component/index.jsx'],
 	output: {
-		path: path.join(__dirname, 'dist'),
+		path: path.join(__dirname, '/'),
 		filename: 'bundle.js',
 		publicPath: '/'
 	},
@@ -31,7 +31,7 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: './index.html',
+			template: './src/index.html',
 		})
 	],
 	mode: 'development'
